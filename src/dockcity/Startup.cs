8dbe1d99cada7dea4140a56bc4ed14a05f64a01b
Shibly <shibly77@outlook.com>
2018-09-12 14:55:16 +0600

    helloo

--- src/dockcity/Startup.cs
+++ src/dockcity/Startup.cs
@@ -24,7 +24,7 @@ namespace dockcity
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Docker deployment test!");
+                await context.Response.WriteAsync("Docker deployment test1!");
             });
         }
     }
